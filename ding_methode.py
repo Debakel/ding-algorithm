731aa4fb6fbe7511200a7e0d06c57f6883453616
Moritz Duchêne <mail@mrtz.me>
2016-02-10 18:06:48 +0100

    bla

--- ding_methode.py
+++ ding_methode.py
@@ -4,8 +4,18 @@
 
 # Tf Feuchttemperatur [°C]
 
+import math
+
 def Tf(Td):
     pass
 
 def saettigungsdampfdruck(Td):
-    return 0.611 * 
+    return 0.611 * math.exp((17.3 * Td / (Td + 237,3))) # Magnus Formel
+
+def steigung(Td):
+    return (2508.3 / (Td + 237.3)**2) * math.exp(17.3*Td / (Td + 237.3))
+
+def luftdruck(z):
+    return 101.3 * math.exp(-0.00013 * z)
+
+T_
