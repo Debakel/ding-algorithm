e9f0b4dfb5c0406a16497a302b6addc7f68bb0af
Moritz Duchêne <mail@mrtz.me>
2016-02-10 18:00:28 +0100

    initial

---
+++ ding_methode.py
@@ -0,0 +1,11 @@
+# z Stationshöhe [m]
+# Td Tagesmitteltemperatur [°C]
+# RH relative Luftfeuchte [Dezimalbruch]
+
+# Tf Feuchttemperatur [°C]
+
+def Tf(Td):
+    pass
+
+def saettigungsdampfdruck(Td):
+    return 0.611 * 
