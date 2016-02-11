7da2f9168ade082e289d117439b315c75436f7ba
Moritz Duchêne <mail@mrtz.me>
2016-02-11 23:51:46 +0100

    Added model

--- dwd/parser.py
+++ dwd/parser.py
@@ -2,6 +2,8 @@
 
 import csv
 import os
+import datetime
+
 # Spalten Metadaten
 STATIONSHOEHE = 1
 VON_DATUM = 4
@@ -14,6 +16,23 @@ LUFTTEMPERATUR = 3
 REL_FEUCHTE = 7
 NIEDERSCHLAGSFORM = 14
 
+# Niederschlagsformen
+KEIN_NIEDERSCHLAG = 0
+NICHT_BEKANNT = 4
+REGEN = 6
+REGEN_VOR1979 = 1 # 1 vor 1979
+SCHNEEREGEN = 8
+SCHNEE = 7
+
+
+class Messwert():
+    stations_id = None
+    date = None
+    lufttemperatur = None
+    rel_feuchte = None
+    niederschlagsform = None
+    stationshoehe = None
+
 def read_csv(filename):
     DELIMETER = ';'
     with open(filename) as csvfile:
@@ -24,13 +43,13 @@ def read_csv(filename):
             rows.append(row)
         return rows
 
-def read_dataset(dirname, filename_metadaten=None, filename_tageswerte=None):
-    # Metadaten in Stationsmetadaten_klima_stationen_00164_20140808_20160208.txt
-    # Tageswerte in produkt_klima_Tageswerte_20140808_20160208_00164.txt
+
+def read_dataset(dirname=None, filename_metadaten=None, filename_tageswerte=None):
+    # Read csv file
     if filename_metadaten is not None and filename_tageswerte is not None:
         metadaten = read_csv(filename_metadaten)
         tageswerte = read_csv(filename_tageswerte)
-    else:
+    elif dirname is not None:
         files = os.listdir(dirname)
         for file in files:
             if file[0] is '.':
@@ -39,19 +58,38 @@ def read_dataset(dirname, filename_metadaten=None, filename_tageswerte=None):
                 metadaten = read_csv(os.path.join(dirname, file))
             elif 'Tageswerte' in file:
                 tageswerte = read_csv(os.path.join(dirname, file))
+    else:
+        return []
 
+    # Loop rows
     data_set = []
     for messung in tageswerte:
         try:
-            data_set.append({
-                'STATIONS_ID': int(messung[STATIONS_ID]),
-                'LUFTTEMPERATUR': float(messung[LUFTTEMPERATUR]),
-                'MESS_DATUM': messung[MESS_DATUM],
-                'REL_FEUCHTE': float(messung[REL_FEUCHTE]) / 100,
-                'NIEDERSCHLAGSFORM': int(messung[NIEDERSCHLAGSFORM]),
-                'STATIONS_HOEHE': int(metadaten[2][STATIONSHOEHE])
-            })
+            m = Messwert()
+            m.date = datetime.datetime.strptime(messung[MESS_DATUM], '%Y%m%d')
+            m.stations_id = int(messung[STATIONS_ID])
+            m.lufttemperatur = float(messung[LUFTTEMPERATUR])
+            m.rel_feuchte = float(messung[REL_FEUCHTE]) / 100
+            m.niederschlagsform = int(messung[NIEDERSCHLAGSFORM])
+            if m.niederschlagsform is REGEN_VOR1979:
+                m.niederschlagsform = REGEN
+
+            # Metadateneintrag wählen (nach Datum)
+            for i in range(1 ,metadaten.__len__()):
+                row = metadaten[i]
+                von = datetime.datetime.strptime(row[VON_DATUM], '%Y%m%d')
+                try:
+                    bis = datetime.datetime.strptime(row[BIS_DATUM], '%Y%m%d')
+                except:
+                    bis = bis = datetime.datetime.now()
+                if von <= m.date <= bis:
+                    break
+
+            m.stationshoehe = int(metadaten[i][STATIONSHOEHE])
+
+            data_set.append(m)
         except:
-            break
+            pass
 
     return data_set
+
